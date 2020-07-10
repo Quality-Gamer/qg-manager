d86c6fb9f8a41f30c48984c95bc269e7835d8bd8
Agualuza <iagoagualuza@id.uff.br>
2020-07-10 19:36:23 -0300

    Update redis.go

--- database/redis.go
+++ database/redis.go
@@ -7,7 +7,7 @@ import (
 )
 
 var client = redis.NewClient(&redis.Options{
-	Addr:     os.Getenv("REDIS_ADDR"),
+	Addr:     os.Getenv("REDISCLOUD_URL"),
 	Password: os.Getenv("REDIS_PASS"), // no password set
 	DB:       0,  // use default DB
 })
